4771862667a8d7e1bcecea8c7709dd5e7db53b33
José Noguera <juniornoguera1@gmail.com>
2025-07-30 17:34:55 -0600

    Avance programa

---
+++ ejercicio-10.py
@@ -0,0 +1,45 @@
+productos = {}
+
+cantidad = int(input("¿Cuantos productos va a ingresar?: "))
+
+for i in range(cantidad):
+    print(f"Producto {i + 1}")
+    code = input("Ingresa código: ")
+    if code in productos:
+        while True:
+            print("Codigo exitente!")
+            code = input("Agregue otro codigo: ")
+            if code not in productos:
+                break
+    name = input("Ingresa nombre: ")
+    category = input("Ingrese la categoría (Hombre, Mujer o Niño): ")
+    size = input("Ingresa la talla (S, M, L o XL): ")
+    price = int(input("Ingresa el precio: "))
+    if price <= 0:
+        while price <= 0:
+            print("Error, precio no válido (Debe ser mayor a 0)")
+            price = int(input("Ingrese nuevamente el precio: "))
+    cant = int(input("Ingrese la cantidad en stock: "))
+    while cant < 0:
+        print("Cantidad incorrecta (negativa)")
+        cant = input("Agrega nuevamente la cantidad: ")
+        if cant > 0:
+            break
+    productos[code] = {
+        "codigo": code,
+        "nombre": name,
+        "categoria": category,
+        "talla": size,
+        "precio": price,
+        "cantidad": cant
+    }
+
+def ShowAllProducts():
+    for code, i in productos.items():
+        print("- - -  Producto ingresado  - - -")
+        print(f"Código: {i['codigo']}")
+        print(f"Nombre: {i['nombre']}")
+        print(f"Categoría: {i['categoria']}")
+        print(f"Talla: {i['talla']}")
+        print(f"Precio: {i['precio']}")
+        print(f"Cantidad en stock: {i['cantidad']}")
