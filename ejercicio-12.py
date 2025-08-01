d61d61796eb5628077d9737c5c46f279918fc396
José Noguera <juniornoguera1@gmail.com>
2025-08-01 09:51:21 -0600

    codigo hecho en clase

---
+++ ejercicio-12.py
@@ -0,0 +1,23 @@
+students = int(input("Cuantos estudiantes desea ingresar: "))
+while True:
+    try:
+        students
+    except ValueError:
+        print("Error, número no valido")
+
+    except TypeError:
+        print("Error, caracter no válido")
+
+    else:
+        print(f"Numero {students} ingresado con éxito")
+
+    for x in range(students):
+        name = input("Ingrese su nombre: ")
+        many_notes = int(input("Ingrese la cantidad de notas: "))
+        note = 0
+        for y in range(many_notes):
+            notes = int(input(f"Ingrese el punteo {y + 1}: "))
+            note += notes
+        print(f"\nNombre: {name}"
+              f"\nPromedio: {(note) / many_notes}\n ")
+
