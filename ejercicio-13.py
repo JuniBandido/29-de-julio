1726140d1caa497e9815b89ba063806d098a24d3
José Noguera <juniornoguera1@gmail.com>
2025-08-04 22:58:31 -0600

    avance de codigo

---
+++ ejercicio-13.py
@@ -0,0 +1,51 @@
+estudiantes = {}
+
+def addStudents():
+    id_student = input("Ingrese el id del estudiante: ")
+    name_student = input("Ingrese el nombre completo del estudiante: ")
+    carrer_student = input("Ingrese la carrera del estudiante: ")
+    print("Estudiante agregado con éxito\n")
+    estudiantes[id_student] = {
+         'id': id_student,
+         'nombre': name_student,
+         'carrera': carrer_student
+     }
+
+def addCurse():
+    soli = input("Ingrese el id del estudiante: ")
+    if soli in estudiantes:
+        name_curse = input("Agregue el nombre del curso: ")
+        note_curse = int(input("Agregue la calificacion del curso: "))
+        estudiantes[soli]['cursos'] = {
+            'nombreCurso': name_curse,
+            'notaCurso': note_curse
+        }
+
+def showStudent():
+    soli = input("\nIngrese el id del estudiante: ")
+    if soli in estudiantes:
+        for soli, i in estudiantes.items():
+            print("-- Estudiante encontrado --")
+            print(f"\nId: {i['id']}")
+            print(f"\nNombre: {i['nombre']}")
+            print(f"\nCarrera: {i['carrera']}")
+            print(f"\nNombre del curso: {i['cursos']['nombreCurso']}")
+            print(f"\nNota de curso: {i['cursos']['notaCurso']}\n")
+
+while True:
+    print("1. Agregar estudiante\n"
+          "2. Agregar curso\n"
+          "3. Mostrar información\n"
+          "4. Salir\n")
+    menu = input("Escoge una opción (1-4): ")
+    match menu:
+        case "1":
+            addStudents()
+        case "2":
+            addCurse()
+        case "3":
+            showStudent()
+        case "4":
+            break
+        case _:
+            print("Opcion no válida\n")
